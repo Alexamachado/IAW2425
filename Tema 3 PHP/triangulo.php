7ae78278a70d755d2767561c59129c7ad0d7a7eb
Alexamachado <alexandroguerrero02@iesamachado.org>
2024-12-05 13:51:21 +0100

    asdasdas

---
+++ Tema 3 PHP/triangulo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangulo</title>
+</head>
+<body>
+    <p>Introduce un numero positivo</p>
+    <input type="number" id="introducido"> <button onclick="trinagulo()">Enviar</button>
+    <?php
+        function triangulo()   
+    ?>
+</body>
+</html>
