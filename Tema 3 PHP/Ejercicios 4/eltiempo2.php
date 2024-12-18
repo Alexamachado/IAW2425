1f9344c74f17288de307f43d8cbbffb140bbbdd8
Alexamachado <alexandroguerrero02@iesamachado.org>
2024-12-18 14:11:51 +0100

    tiempo 2 corregido

--- Tema 3 PHP/Ejercicios 4/eltiempo2.php
+++ Tema 3 PHP/Ejercicios 4/eltiempo2.php
@@ -1,5 +1,7 @@
 <?php
-    $text=file_get_contents("https://www.eltiempo.es/sevilla.html");
-    $resultado = explode("",$text)                   // No funciona
-    print_r($resultado[4]);
+    //$text=file_get_contents("https://www.eltiempo.es/sevilla.html");
+    $tiempo = "https://www.eltiempo.es/sevilla.html"
+    $text=file_get_contents($tiempo);
+    $resultado = explode("<span class='degrees' data-temperature=",$text);                   // No funciona
+    print_r($resultado[0]);
 ?>
