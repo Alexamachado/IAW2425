9c74fb47999388aed59d723b0b642da0e0e53fa3
Alexamachado <alexandroguerrero02@iesamachado.org>
2025-01-10 00:01:36 +0100

    Jueves 9 enero - 3

--- Tema 3 PHP/Ejercicios 3/sorteo.php
+++ Tema 3 PHP/Ejercicios 3/sorteo.php
@@ -1,5 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<style>
+</style>
+<title> sorteo </title>
+</head>
+<body>
 
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+Ve introduciendo los usuario  <input type="text" name="nombres"><br>
+Añadir usuario<input type="submit" name="anadir"> &nbsp; &nbsp;  Realizar sorteo<input type="submit" name="sorteo">
+</form> 
 
+<?php
+if (isset($_POST["nombres"])){
+$nombres = array($_POST["nombres"]);
+$anadidos = explode(",", $_POST["nombres"]);
+$nombres = array_merge($nombres, $anadidos);
+
+//array_push($nombres, $_POST["nombres"]);
+print_r($nombres);
+}
+?>
+</body>
+</html>
 
 <!-- sorteo.php. Crea un formulario que te permita sortear un premio entre el número de 
 participantes que el usuario introduzca en una caja de texto; esto es, el usuario envía 
--- Tema 3 PHP/Ejercicios 4/cookieSegura.php
+++ Tema 3 PHP/Ejercicios 4/cookieSegura.php
@@ -1,14 +1,7 @@
-<!-- En base al ejercicio anterior, modifica las cookies desde el navegador web con 
-alguna extensión propia del navegador que estés utilizando; por ejemplo, la popular 
-extensión Web Developer de chrispederick.com. ¿Qué problemas pueden existir si el 
-“lado cliente” puede “modificar” las cookies de sesión?¿Cómo podemos evitar este riesgo en PHP?
-Realiza un script de nombre cookieSegura.php que impida que el “lado cliente” modifique las 
-cookies que envía el servidor. -->
-
 <?php
 $cookie_name = "user";
 $cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/",,,true); // 86400 = 1 day
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/","",false,true); // 86400 = 1 day
 
 ?>
 
@@ -25,3 +18,10 @@ if(!isset($_COOKIE[$cookie_name])) {
 
 </body>
 </html> 
+
+<!-- En base al ejercicio anterior, modifica las cookies desde el navegador web con 
+alguna extensión propia del navegador que estés utilizando; por ejemplo, la popular 
+extensión Web Developer de chrispederick.com. ¿Qué problemas pueden existir si el 
+“lado cliente” puede “modificar” las cookies de sesión?¿Cómo podemos evitar este riesgo en PHP?
+Realiza un script de nombre cookieSegura.php que impida que el “lado cliente” modifique las 
+cookies que envía el servidor. -->
