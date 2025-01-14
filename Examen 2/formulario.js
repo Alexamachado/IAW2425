59136fda9bf3e8432389a6f5d8b602c814fa1cfd
Alex <alexandroguerrero02@iesamachado.org>
2025-01-14 06:48:59 +0100

    Lunes 13 enero - 2

--- Examen 2/formulario.js
+++ Examen 2/formulario.js
@@ -1,19 +1,73 @@
-function asunto(){
-    let asunto= document.getElementById("asunto").value
-    if (asunto==''){
-     document.getElementById("error1").innerHTML = "El campo Asunto es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error1").innerHTML = ""
-         return true}
- }
+function comprobar(){
+   let asunto = document.getElementById("asunto").value
+   let dni = document.getElementById("dni").value
+   let nombre = document.getElementById("nombre").value
+   let apellido = document.getElementById("apellido1").value
+   let fecha = document.getElementById("fecha").value
+   let telefono = document.getElementById("telefono").value
+   let email = document.getElementById("correo").value
+   let domicilio = document.getElementById("domicilio").value
+   let postal = document.getElementById("postal").value
+   let municipio = document.getElementById("municipio").value
+   let oficina = document.getElementById("oficina").value
+   let requerido = document.getElementById("requerido").value
+   let caja = document.getElementById("asunto").checked
 
- function dni(){
-    let dn= document.getElementById("dni").value
-    if (dn==''){
-     document.getElementById("error2").innerHTML = "El campo DNI/NIE es necesario para completar el formulario"
-     return false
-    }else if(dn!=''){
+    var errores = document.querySelectorAll(".error"); //document.getElementsByClassName("error")
+    for (var i = 0; i < errores.length; i++) {
+        errores[i].innerHTML = "";}
+
+    if(asunto ==""){
+        document.getElementById("campo1").innerHTML= "El campo Asunto es necesario para completar el formulario"
+        allok = false
+    }if(dni ==""){
+        document.getElementById("campo2").innerHTML= "El campo DNI es necesario para completar el formulario"
+        allok = false
+    }else if(!validardni()){
+        document.getElementById("campo2").innerHTML= "El DNI introducido es incorrecto"
+        allok = false
+    }if(nombre ==""){
+        document.getElementById("campo3").innerHTML= "El campo Nombre es necesario para completar el formulario"
+        allok = false
+    }if(apellido ==""){
+        document.getElementById("campo4").innerHTML= "El campo Apellido es necesario para completar el formulario"
+        allok = false
+    }if(fecha ==""){
+        document.getElementById("campo6").innerHTML= "El campo Fecha es necesario para completar el formulario"
+        allok = false
+    }if(telefono ==""){
+        document.getElementById("campo7").innerHTML= "El campo Telefono es necesario para completar el formulario"
+        allok = false
+    }if(!validaremail()){
+        document.getElementById("campo8").innerHTML= "Debes de introducir un correo valido"
+        allok = false
+    }if(domicilio==""){
+        document.getElementById("campo9").innerHTML= "El campo Asunto es necesario para completar el formulario"
+        allok = false
+    }if(postal ==""){
+        document.getElementById("campo10").innerHTML= "El campo Codigo Postal es necesario para completar el formulario"
+        allok = false
+    }if(municipio ==""){
+        document.getElementById("campo11").innerHTML= "El campo Municipio es necesario para completar el formulario"
+        allok = false
+    }if(oficina ==""){
+        document.getElementById("campo12").innerHTML= "El campo Oficina es necesario para completar el formulario"
+        allok = false
+    }if(requerido ==""){
+        document.getElementById("campo13").innerHTML= "El campo Información requerida es necesario para completar el formulario"
+        allok = false
+    }if(!caja){
+        document.getElementById("campo16").innerHTML= "El campo Politica de datos es necesario para completar el formulario"
+        allok = false
+    }
+
+    if (allok){
+
+        
+    }
+} 
+
+    function validardni(){
         let letra =['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
         var cadena = document.getElementById("dni").value; //DNI completo 
         let cadenaSinEspacios = cadena.replace(" ", "");   
@@ -21,149 +75,33 @@ function asunto(){
         var numero = parseInt(cadenaLimpia.substring(0,8)); //Parte numerica
         var letraUsuario = cadena [8]; //Letra escrita por el usuario
         var letraReal = letra[numero%23]; // Letra "real" del DNI calculada segun formula
-        if (letraUsuario!=letraReal){  // Si no coincide letras es falso
-            document.getElementById("error2").innerHTML = "DNI incorrecto"
-            return false;} 
-        else{
-            document.getElementById("error2").innerHTML = ""
-            return true;}
-        }
- }
-
-function nombre(){
-   let name= document.getElementById("nombre").value
-   if (name==''){
-    document.getElementById("error3").innerHTML = "El campo nombre es necesario para completar el formulario"
-    return false
-    }else {
-        document.getElementById("error3").innerHTML = ""
-        return true}
-}
-
-function apellidos(){
-    let apellido1 = document.getElementById("apellido1").value
-    if(apellido1==''){
-        document.getElementById("error4").innerHTML = "El campo apellidos es necesario para completar el formulario"
-     }else {
-        document.getElementById("error4").innerHTML = ""
-        return true}
- }
-
- function fecha(){
-    let fecha= document.getElementById("fecha").value
-    if (fecha==''){
-     document.getElementById("error5").innerHTML = "El campo fecha es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error5").innerHTML = ""
-         return true}
- }
-
- function telefono(){
-    let telefono= document.getElementById("telefono").value
-    if (telefono==''){
-     document.getElementById("error6").innerHTML = "El campo telefono es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error6").innerHTML = ""
-         return true}
- }
-
- function email(){
-    let email= document.getElementById("email").value
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(email)==false) {
-        document.getElementById("error7").innerHTML = "El campo email es necesario para completar el formulario"
-        return false
-     }else {
-        document.getElementById("error7").innerHTML = ""
-        return true}
- }
-
- function domicilio(){
-    let domicilio= document.getElementById("domicilio").value
-    if (domicilio==''){
-     document.getElementById("error8").innerHTML = "El campo domicilio es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error8").innerHTML = ""
-         return true}
- }
-
- function postal(){
-    let postal= document.getElementById("postal").value
-    if (postal==''){
-     document.getElementById("error9").innerHTML = "El campo postal es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error9").innerHTML = ""
-         return true}
- }
-
- function municipio(){
-    let municipio= document.getElementById("municipio").value
-    if (municipio==''){
-     document.getElementById("error10").innerHTML = "El campo municipio es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error10").innerHTML = ""
-         return true}
- }
-
- function oficina(){
-    let oficina= document.getElementById("oficina").value
-    if (oficina==''){
-     document.getElementById("error11").innerHTML = "El campo oficina es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error11").innerHTML = ""
-         return true}
- }
-
- function descripcion(){
-    let descripcion= document.getElementById("descripcion").value
-    if (descripcion==''){
-     document.getElementById("error12").innerHTML = "El campo Informacion es necesario para completar el formulario"
-     return false
-     }else {
-         document.getElementById("error12").innerHTML = ""
-         return true}
- }
-
- function anexo(){
-   /* let anexo= document.getElementById("asunto").value
-    if (asunto==''){
-     document.getElementById("error13").innerHTML = "Debes rellenar el asunto"
-     return false
-     }else {
-         document.getElementById("error13").innerHTML = ""
-         return true}*/
- }
+        if (letraUsuario!=letraReal){return false;} 
+        else{return true;}
+        }    
 
 
- function caja(){
-    let caja = document.getElementById("caja").checked
-    if (caja == false){
-        document.getElementById("error15").innerHTML = "El campo Politicas es necesario para completar el formulario"
-        return false
-    }else{
-        document.getElementById("error15").innerHTML = ""
-        return true}
-}
+    
+    function validaremail(){
+        var correo = document.getElementById("correo").value;
+        var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        if(correo == ""){return true}
+        else if(!validEmail.test(correo) ){return false;}
+        else {return true;}
+    }
+    
+    function limitearchivo(){
+        fila1 = document.getElementById("anexo1")
+        fila2 = document.getElementById("anexo2")
+        /*
+        const uploadField = document.getElementById("file");
 
-function comprobar(){
-    asunto();
-    nombre();
-    apellidos();
-    fecha();
-    telefono();
-    domicilio();
-    municipio();
-    postal();
-    oficina();
-    anexo();
-    dni();
-    email();
-    descripcion();
-    caja();    
-}
+        uploadField.onchange = function() {
+              if(this.files[0].size > 2097152) {
+                alert("File is too big!");
+                this.value = "";
+            }
+        };
+        */
+        if(){}
+        
+    }
